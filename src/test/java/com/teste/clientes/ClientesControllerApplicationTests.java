76ce72a0095fa350f33f41c5b76742ce3725aab7
Marcelo Anderson <marceloanderson@pactosolucoes.com.br>
2021-02-08 15:26:48 -0300

    Alterando testes unitarios

--- src/test/java/com/teste/clientes/ClientesControllerApplicationTests.java
+++ src/test/java/com/teste/clientes/ClientesControllerApplicationTests.java
@@ -1,27 +1,13 @@
 package com.teste.clientes;
 
-import com.teste.clientes.controllers.ClienteController;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = ClienteController.class)
+@SpringBootTest
 class ClientesControllerApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-
 	@Test
-	public void findAllUsers() throws Exception {
-		this.mockMvc.perform(get("/clientes"))
-				.andExpect(status().isOk());
+	void contextLoads() {
 	}
 
 }
